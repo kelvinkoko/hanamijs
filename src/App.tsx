b5b84521935a3bd726ef2f864b56fc139fa73cff
kelvinko <kelvinkoko@gmail.com>
2022-04-01 01:07:47 +0900

    Add canvas with draw line function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
+import Canvas from "./canvas/Canvas";
 
 class App extends React.Component {
   render() {
+    const canvas = new Canvas("canvas");
+    canvas.drawLine(0, 0, 100, 100);
     return (
       <>
         <h1>Hello</h1>
---
+++ src/canvas/Canvas.ts
@@ -0,0 +1,34 @@
+import { Layer } from "konva/lib/Layer";
+import { Line } from "konva/lib/shapes/Line";
+import { Stage } from "konva/lib/Stage";
+
+export default class Canvas {
+  private readonly layer: Layer;
+
+  constructor(canvasContainerId: string) {
+    this.layer = this.setupKonvaLayer(canvasContainerId);
+  }
+
+  drawLine(x1: number, y1: number, x2: number, y2: number) {
+    var redLine = new Line({
+      points: [x1, y1, x2, y2],
+      stroke: "#ff00ff",
+      strokeWidth: 15
+    });
+    this.layer.add(redLine);
+  }
+
+  private setupKonvaLayer = (containerId: string): Layer => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    const stage = new Stage({
+      container: containerId,
+      width: width,
+      height: height
+    });
+    const layer = new Layer();
+    stage.add(layer);
+    return layer;
+  };
+}
