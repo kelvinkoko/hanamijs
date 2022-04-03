31f967f9817664a922bd2f5b5b11f5b6ec7671c5
kelvinko <kelvinkoko@gmail.com>
2022-04-04 00:43:08 +0900

    Change App to FC style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect } from "react";
 import { hot } from "react-hot-loader/root";
 import styles from "./App.module.css";
 import Canvas from "./canvas/Canvas";
@@ -7,59 +8,58 @@ import LSystem from "./lsystem/LSystem";
 import PlantModel from "./lsystem/PlantModel";
 import Preset from "./lsystem/Preset";
 import Rule from "./lsystem/Rule";
-class App extends React.Component {
-  private canvas: Canvas | undefined;
-  private plant: PlantModel | undefined;
 
-  render() {
-    return (
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <div
-            className={styles.refreshButton}
-            onClick={() => {
-              if (this.canvas && this.plant) {
-                this.draw(this.canvas, this.plant);
-              }
-            }}
-          >
-            🌸 生成する
-          </div>
+const App = () => {
+  const PLANT: PlantModel = Preset.SAKURA;
+  const CANVAS_CONTAINER_ID = "canvas";
+  let canvas: Canvas | undefined;
+
+  useEffect(() => {
+    canvas = new Canvas(CANVAS_CONTAINER_ID, 600, 500);
+    draw(canvas, PLANT);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div
+          className={styles.refreshButton}
+          onClick={() => {
+            if (canvas) {
+              draw(canvas, PLANT);
+            }
+          }}
+        >
+          🌸 生成する
         </div>
-        <div id="canvas" />
       </div>
-    );
-  }
+      <div id={CANVAS_CONTAINER_ID} />
+    </div>
+  );
+};
 
-  componentDidMount() {
-    this.canvas = new Canvas("canvas", 600, 500);
-    this.plant = Preset.SAKURA;
-    this.draw(this.canvas, this.plant);
-  }
+const draw = (canvas: Canvas, model: PlantModel) => {
+  canvas.clear();
+  const expression = generateExpression(
+    model.axiom,
+    model.rules,
+    model.defaultIteration
+  );
+  const interpreter = new Interpreter(canvas);
+  interpreter.interpret(expression);
+};
 
-  private draw(canvas: Canvas, model: PlantModel) {
-    canvas.clear();
-    const expression = this.generateExpression(
-      model.axiom,
-      model.rules,
-      model.defaultIteration
-    );
-    const interpreter = new Interpreter(canvas);
-    interpreter.interpret(expression);
+const generateExpression = (
+  axiom: string,
+  rules: Rule[],
+  iteration: number
+): string => {
+  const lSystem = new LSystem();
+  let expression = axiom;
+  for (let i = 0; i < iteration; i++) {
+    expression = lSystem.applyRules(expression, rules);
   }
-
-  private generateExpression(
-    axiom: string,
-    rules: Rule[],
-    iteration: number
-  ): string {
-    const lSystem = new LSystem();
-    let expression = axiom;
-    for (let i = 0; i < iteration; i++) {
-      expression = lSystem.applyRules(expression, rules);
-    }
-    return expression;
-  }
-}
+  return expression;
+};
 
 export default hot(App);
