38fd07fd5a26d5691f8ccdd7dbb7d1f91a0644a7
kelvinko <kelvinkoko@gmail.com>
2022-04-06 22:41:19 +0900

    Add config panel

---
+++ src/ui/ConfigPanel.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import styles from "./ConfigPanel.module.css";
+
+const ConfigPanel = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default ConfigPanel;
